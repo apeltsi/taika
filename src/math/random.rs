bc2f0321b46787b2bdbe50d2914bbb1c97fb519c
Amos Peltonen <49206921+apeltsi@users.noreply.github.com>
2024-06-12 16:08:33 +0200

    feat: random range now accepts range of any type

--- src/math/random.rs
+++ src/math/random.rs
@@ -1,5 +1,13 @@
-use rand::Rng;
-pub fn random_in_range(min: f32, max: f32) -> f32 {
+use rand::{
+    distributions::uniform::{SampleRange, SampleUniform},
+    Rng,
+};
+/// Generate a random number in the range.
+pub fn range<T, R>(range: R) -> T
+where
+    T: SampleUniform,
+    R: SampleRange<T>,
+{
     let mut rng = rand::thread_rng();
-    rng.gen_range(min..max)
+    rng.gen_range(range)
 }
