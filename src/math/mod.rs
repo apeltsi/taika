eb0598d7b4d5221f11b2bcd9f664a2407c85f953
apeltsi <49206921+apeltsi@users.noreply.github.com>
2024-06-13 01:37:31 +0200

    feat: more math functions

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -6,3 +6,11 @@ pub const IDENTITY_MATRIX: [[f32; 4]; 4] = [
     [0.0, 0.0, 1.0, 0.0],
     [0.0, 0.0, 0.0, 1.0],
 ];
+
+pub fn lerp(a: f32, b: f32, t: f32) -> f32 {
+    a + (b - a) * t
+}
+
+pub fn lerp64(a: f64, b: f64, t: f64) -> f64 {
+    a + (b - a) * t
+}
